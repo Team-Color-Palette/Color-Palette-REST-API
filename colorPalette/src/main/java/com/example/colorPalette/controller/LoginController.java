9970e3450d0236e13006984ce55d5f7f313a98c6
김동윤 <85720792+kdy9416@users.noreply.github.com>
2021-12-28 22:52:49 +0900

    로그인 구현

--- colorPalette/src/main/java/com/example/colorPalette/controller/LoginController.java
+++ colorPalette/src/main/java/com/example/colorPalette/controller/LoginController.java
@@ -2,7 +2,12 @@ package com.example.colorPalette.controller;
 
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +23,58 @@ public class LoginController {
 	@Autowired 
 	LoginService service;
 	
+	//회원가입
 	@PostMapping("/")
 	public String insert(@RequestBody UserVO vo) {
-		service.insert(vo);
-		return "joinSuccess";
+		int i = service.checkId(vo.getId());
+		if(i > 0) {
+			service.insert(vo);		
+			return "joinSuccess";
+		} else {
+			return "joinFail";
+		}
+		
+	}
+	
+	//회원탈퇴
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable String id) {
+		return "deleteSuccess";
+	}
+	
+	//회원조회
+	@GetMapping("/{id}")
+	public UserVO select(@PathVariable String id ) {
+		
+		return service.select(id);
+	}
+	
+	//로그인
+	@PostMapping("/loginCheck")
+	public String login(@RequestBody UserVO input , HttpSession session) {
+		UserVO vo = service.select(input.getId());
+		if(vo != null) {
+			if(vo.getPassword().equals(input.getPassword())) {
+				session.setAttribute("login", vo);
+				return "loginSuccess";
+			}else {
+				return "pwFail";
+			}
+		}else {
+			return "idFail";
+		}
+	}
+	
+	//로그아웃
+	@PostMapping("/logout")
+	public String logout(HttpSession session) {
+		UserVO vo = (UserVO)session.getAttribute("login");
+		if(vo!=null) {
+			session.removeAttribute("login");
+			session.invalidate();
+			return "logoutSuccess";
+		}
+		return "logoutFail";
+		
 	}
 }
